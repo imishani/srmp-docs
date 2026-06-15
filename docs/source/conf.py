d261b9aa28e29d9faf8bc1cccb0f1aff9f3f4d41
Itamar Mishani <imishani@gmail.com>
2026-06-15 10:17:33 -0400

    theme change: furo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -29,7 +29,7 @@ templates_path = ['_templates']
 
 # -- Options for HTML output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'furo'
 
 # Copy static files from assets directory
 html_static_path = ['_static']
